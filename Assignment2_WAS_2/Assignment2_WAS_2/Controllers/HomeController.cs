9ef067019bb24a2513a5a499242ed745e1c72c3a
jessebrightman <snowboarding1>
2015-02-16 15:36:42 -0800

    updated project and submitted to pat

--- Assignment2_WAS_2/Assignment2_WAS_2/Controllers/HomeController.cs
+++ Assignment2_WAS_2/Assignment2_WAS_2/Controllers/HomeController.cs
@@ -54,7 +54,11 @@ namespace Assignment2_WAS_2.Controllers
                     return RedirectToAction("Welcome", "Home");
                 }
             }
-            ViewBag.Message = "<div class='alert alert-danger form-group' role='alert'>Please Register before Logging In.</div>";
+            else 
+            {
+                ViewBag.Message = "<div class='alert alert-danger form-group'   role='alert'>Please Register before Logging In.</div>";
+            }
+            
             return View();
         }
         [HttpGet]
@@ -66,46 +70,49 @@ namespace Assignment2_WAS_2.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(RegisteredUser newUser)
         {
-            var userStore = new UserStore<IdentityUser>();
-            var manager = new UserManager<IdentityUser>(userStore);
-            var identityUser = new IdentityUser()
+            if (newUser != null)
             {
-                UserName = newUser.UserName,
-                Email = newUser.Email,
-                PhoneNumber = newUser.PhoneNumber
-            };
-
-            IdentityResult result = manager.Create(identityUser, newUser.Password);
-
-            if (result.Succeeded)
-            {
-                var authenticationManager
-                                  = HttpContext.Request.GetOwinContext().Authentication;
-                var userIdentity = manager.CreateIdentity(identityUser,
-                                           DefaultAuthenticationTypes.ApplicationCookie);
-                authenticationManager.SignIn(new AuthenticationProperties() { },
-                                             userIdentity);
-
-                MyUser myUser = new MyUser();
-
-                myUser.myUser = newUser.UserName;
-                myUser.myEmail = newUser.Email;
-                myUser.myAddress = newUser.Address;
-                myUser.phone = newUser.PhoneNumber;
-                myUser.city = newUser.City;
-                myUser.province = newUser.State;
-                myUser.country = newUser.Country;
-
-                db.MyUsers.Add(myUser);
-
-                db.SaveChanges();
-                ViewBag.Message = "<div class='alert alert-success form-group' role='alert'>You have been Registered.  Please Login.</div>";
-            }
-            else 
-            {
-                foreach (var error in result.Errors)
+                var userStore = new UserStore<IdentityUser>();
+                var manager = new UserManager<IdentityUser>(userStore);
+                var identityUser = new IdentityUser()
                 {
-                    ViewBag.Message = "<div class='alert alert-danger form-group' role='alert'>Registration failed. " + error + ". Please Register again.</div>";
+                    UserName = newUser.UserName,
+                    Email = newUser.Email,
+                    PhoneNumber = newUser.PhoneNumber
+                };
+
+                IdentityResult result = manager.Create(identityUser, newUser.Password);
+
+                if (result.Succeeded)
+                {
+                    var authenticationManager
+                                      = HttpContext.Request.GetOwinContext().Authentication;
+                    var userIdentity = manager.CreateIdentity(identityUser,
+                                                                DefaultAuthenticationTypes.ApplicationCookie);
+                    authenticationManager.SignIn(new AuthenticationProperties() { },
+                                                 userIdentity);
+
+                    MyUser myUser = new MyUser();
+
+                    myUser.myUser = newUser.UserName;
+                    myUser.myEmail = newUser.Email;
+                    myUser.myAddress = newUser.Address;
+                    myUser.phone = newUser.PhoneNumber;
+                    myUser.city = newUser.City;
+                    myUser.province = newUser.State;
+                    myUser.country = newUser.Country;
+
+                    db.MyUsers.Add(myUser);
+
+                    db.SaveChanges();
+                    ViewBag.Message = "<div class='alert alert-success form-group' role='alert'>You have been Registered.  Please Login.</div>";
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ViewBag.Message = "<div class='alert alert-danger form-group' role='alert'>Registration failed. " + error + ". Please Register again.</div>";
+                    }
                 }
             }
             return View();
@@ -126,16 +133,21 @@ namespace Assignment2_WAS_2.Controllers
         [HttpPost]
         public ActionResult CreateRole(AspNetRole role)
         {
-            try
+            if (db.AspNetRoles.Find(role.Id)==null)
+                try
+                {
+                    db.AspNetRoles.Add(role);
+                    db.SaveChanges();
+                    ViewBag.Message = "<div class='alert alert-success form-group' role='alert'>You have successfully added the " + role.Name + " Role.</div>";
+                }
+                catch (Exception e)
+                {
+                    string error = e.ToString();
+                    ViewBag.Message = "<div class='alert alert-danger form-group' role='alert'>Role creation failed. " + error + ". Please try again.</div>";
+                }
+            else
             {
-                db.AspNetRoles.Add(role);
-                db.SaveChanges();
-                ViewBag.Message = "<div class='alert alert-success form-group' role='alert'>You have successfully added the Role.</div>";
-            }
-            catch (SqlException e)
-            {
-                string error = e.ToString();
-                ViewBag.Message = "<div class='alert alert-danger form-group' role='alert'>Role creation failed. " + error + ". Please try again.</div>";
+                ViewBag.Message = "<div class='alert alert-danger form-group' role='alert'>Role creation failed. " + role.Name + " already exists.</div>";
             }
             return View();
         }
@@ -147,15 +159,50 @@ namespace Assignment2_WAS_2.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult ModifyUserRole(string userName, string roleName)
+        public ActionResult ModifyUserRole(string userName, string roleName, string button)
         {
-            AspNetUser user = db.AspNetUsers
-                             .Where(u => u.UserName == userName).FirstOrDefault();
-            AspNetRole role = db.AspNetRoles
-                             .Where(r => r.Name == roleName).FirstOrDefault();
-
-            user.AspNetRoles.Add(role);
-            db.SaveChanges();
+            
+            try
+            {
+                if (button == "Create")
+                {
+                    AspNetUser user = db.AspNetUsers
+                                     .Where(u => u.UserName == userName).FirstOrDefault();
+                    AspNetRole role = db.AspNetRoles
+                                     .Where(r => r.Name == roleName).FirstOrDefault();
+                    if (user.AspNetRoles.Contains(role) != true)
+                    {
+                        user.AspNetRoles.Add(role);
+                        db.SaveChanges();
+                        ViewBag.Message = "<div class='alert alert-success form-group' role='alert'>You have successfully added " + userName + " to the " + roleName + " Role.</div>";
+                    }
+                    else
+                    {
+                        ViewBag.Message = "<div class='alert alert-danger form-group' role='alert'>Modify User Role failed. " + userName + " already belongs to the " + roleName + " Role.</div>";
+                    }
+                }
+                if (button == "Delete")
+                {
+                    AspNetUser user = db.AspNetUsers
+                                     .Where(u => u.UserName == userName).FirstOrDefault();
+                    AspNetRole role = db.AspNetRoles
+                                     .Where(r => r.Name == roleName).FirstOrDefault();
+                    if (user.AspNetRoles.Contains(role) == true)
+                    {
+                        user.AspNetRoles.Remove(role);
+                        db.SaveChanges();
+                        ViewBag.Message = "<div class='alert alert-success form-group' role='alert'>You have successfully deleted " + userName + " from the " + roleName + " Role.</div>";
+                    }
+                    else
+                    {
+                        ViewBag.Message = "<div class='alert alert-danger form-group' role='alert'>Modify User Role failed. " + userName + " does not belong to the " + roleName + " Role.</div>";
+                    }
+                }
+            }
+            catch
+            {
+                ViewBag.Message = "<div class='alert alert-danger form-group' role='alert'>Failed to delete " + userName + " from the " + roleName + " Role. Please try again.</div>";
+            }
             return View();
         }
 
--- Assignment2_WAS_2/Assignment2_WAS_2/ViewModels/UserRole.cs
+++ Assignment2_WAS_2/Assignment2_WAS_2/ViewModels/UserRole.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Assignment2_WAS_2.ViewModels
 {
     public class UserRole
     {
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
+
+        [Display(Name = "Role Name")]
         public string RoleName { get; set; }
     }
 }
